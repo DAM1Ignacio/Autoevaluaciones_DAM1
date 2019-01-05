bf8506d221b97f1075cc494003f26b8a148a6b3a
Nacho <belmonteperona@gmail.com>
2019-01-05 16:23:45 +0100

    datos de usuarios de prueba generados hasta llenar la array datosUsuario, eliminacion del metodo generarUsuarioPrueba

--- Autoevaluacion/src/autoeval4/Principal.java
+++ Autoevaluacion/src/autoeval4/Principal.java
@@ -9,10 +9,9 @@ public class Principal {
 	static Usuario[] datosUsuarios = new Usuario[MAX_USUARIOS];
 	static Usuario[] datosUsuarioParaCargar;
 	private static int posicionEnElPatron = 0;
-	private static int posicionEnDatosUsuarioParaCargar = 0;
+	private static int numUsuarioParaCargar = 1;
 
 	public static void main(String[] args) {
-		crearDatosUsuarioPrueba();
 		int[] patron = { 1, 1, 1, 0, 1 };
 		cargarDatosUsuariosPatron(patron);
 
@@ -24,55 +23,26 @@ public class Principal {
 	}
 
 	/**
-	 * Metodo utilizado para crear datos de prueba de usuarios
-	 */
-	private static void crearDatosUsuarioPrueba() {
-		String[] nifPrueba = { "00001a", "00002b", "00003c", "000004d", "00005e" };
-		String[] nombrePrueba = { "usuario1", "usuario2", "usuario3", "usuario4", "usuario5" };
-		String[] apellidosPrueba = { "apellido3", "apellido2", "apellido3", "apellido4", "apellido5" };
-		String[] domicilioPrueba = { "calle1", "calle2", "calle3", "calle4", "calle5" };
-		String[] correoPrueba = { "cooreo@1", "cooreo@2", "cooreo@3", "cooreo@4", "correo@5" };
-		String[] fechaNacimientoPrueba = { "1-2-3", "4-5-6", "1-2-3", "4-5-6", "2-4-5" };
-		String[] fechaAltaPrueba = { "1-2-3", "4-5-6", "1-2-3", "4-5-6", "2-4-5" };
-		String[] claveAccesoPrueba = { "Miau#1", "Miau#2", "Miau#3", "Miau#4", "Miau#5" };
-		String[] rolPrueba = { "NORMAL", "NORMAL", "NORMAL", "NORMAL", "NORMAL" };
-		datosUsuarioParaCargar = new Usuario[nombrePrueba.length];
-		for (int i = 0; i < nombrePrueba.length; i++) {
-			datosUsuarioParaCargar[i] = new Usuario(nifPrueba[i], nombrePrueba[i], apellidosPrueba[i], correoPrueba[i],
-					domicilioPrueba[i], fechaNacimientoPrueba[i], fechaAltaPrueba[i], claveAccesoPrueba[i],
-					rolPrueba[i]);
-		}
-	}
-
-	/**
-	 * Este metodo carga los datos de prueba de usuario en la array datosUsuarios segun
-	 * un patron. El patron es una array que dice en que posiciones se pueden
-	 * guardas datos (el valor es 1) y en cuales no (el valor es 0) Este metodo se
-	 * termina cuando se han cargado todos los usuarios de prueba.
+	 * Este metodo carga datos de prueba en la array datosUsuarios segun un patron.
+	 * El patron es una array que dice en que posiciones se pueden guardar datos (el
+	 * valor es 1) y en cuales no (el valor es 0)
 	 * 
 	 * @author Ignacio Belmonte
 	 * @param patron
 	 */
 	private static void cargarDatosUsuariosPatron(int[] patron) {
-		for (int posicionEnArrayDatosUsuarios = 0; posicionEnArrayDatosUsuarios < datosUsuarios.length; posicionEnArrayDatosUsuarios++) {
-			if (isCargadosTodosLosDatosParaCargar()) {
-				break;
-			}
+		for (int posicionDatosUsuarios = 0; posicionDatosUsuarios < datosUsuarios.length; posicionDatosUsuarios++) {
 			if (isRecoridoElPatronCompleto(patron)) {
 				reiniciarPosicionEnElPatron();
 			}
 			if (isPosicionEnElPatronMarcadaParaGuardarDatos(patron)) {
-				cargarDatosEnDatosUsuarios(posicionEnArrayDatosUsuarios);
-				posicionEnDatosUsuarioParaCargar++;
+				cargarDatosEnDatosUsuarios(posicionDatosUsuarios);
+				numUsuarioParaCargar++;
 			}
 			posicionEnElPatron++;
 		}
 		reiniciarPosicionEnElPatron();
 		reiniciarPosicionEnDatosUsuarioParaCargar();
-	}
-
-	private static boolean isCargadosTodosLosDatosParaCargar() {
-		return posicionEnDatosUsuarioParaCargar == datosUsuarioParaCargar.length;
 	}
 
 	private static boolean isRecoridoElPatronCompleto(int[] patron) {
@@ -88,11 +58,15 @@ public class Principal {
 	}
 
 	private static void cargarDatosEnDatosUsuarios(int posicionGuardar) {
-		datosUsuarios[posicionGuardar] = datosUsuarioParaCargar[posicionEnDatosUsuarioParaCargar];
+		datosUsuarios[posicionGuardar] = new Usuario("nif" + numUsuarioParaCargar + numUsuarioParaCargar,
+				" nombre" + numUsuarioParaCargar, " apellidos" + numUsuarioParaCargar, " correo" + numUsuarioParaCargar,
+				" domicilio" + numUsuarioParaCargar, " fechaNacimiento" + numUsuarioParaCargar,
+				" fechaAlta" + numUsuarioParaCargar, " claveAcceso" + numUsuarioParaCargar,
+				"rol" + numUsuarioParaCargar);
 	}
 
 	private static void reiniciarPosicionEnDatosUsuarioParaCargar() {
-		posicionEnDatosUsuarioParaCargar = 0;
+		numUsuarioParaCargar = 0;
 	}
 
 } // class
